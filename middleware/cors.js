6f943c28f51011d7f191e37342bd577f2ee2e50b
DSTechnical-09 <91273646+DSTechnical-09@users.noreply.github.com>
2022-06-03 11:14:27 +0530

    update node demo first app

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,11 +1,11 @@
 const { WHITE_LIST } = require('../config/constant')
 
 var corsOptionsDelegate = function (req, callback) {
-  var corsOptions = { origin: "https://node-demo-first.herokuapp.com/", credentials: true }
+  var corsOptions = { origin: true, credentials: true }
   if (WHITE_LIST.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: "https://node-demo-first.herokuapp.com/", credentials: true }
+    corsOptions = { origin: true, credentials: true }
   } else {
-    corsOptions = { origin: "https://node-demo-first.herokuapp.com/" }
+    corsOptions = { origin: false }
   }
   callback(null, corsOptions)
 }
