73f644ef36f6e9ab94653487503341f4ee8d3b9f
DSTechnical-09 <91273646+DSTechnical-09@users.noreply.github.com>
2022-06-03 11:26:06 +0530

    update node demo first app

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,7 +2,7 @@ const { WHITE_LIST } = require('../config/constant')
 
 var corsOptionsDelegate = function (req, callback) {
   var corsOptions = { origin: true, credentials: true }
-  if (WHITE_LIST.indexOf(req.header('Origin')) !== -1) {
+  if (WHITE_LIST.indexOf(req.header('origin')) !== -1) {
     corsOptions = { origin: true, credentials: true }
   } else {
     corsOptions = { origin: false }
